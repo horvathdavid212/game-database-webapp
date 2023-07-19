8ed43f9dfb9597f57da999b1977cb4e8432b7077
Horváth Dávid <horvath.david212@gmail.com>
2023-07-19 23:01:05 +0200

    kép nélküli játékok ->blank képpel

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImage from "../assets/blank.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImage;
   const traget = "media/";
   const index = url.indexOf(traget) + traget.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
